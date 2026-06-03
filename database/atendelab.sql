e59debdba7539ebbd144ed7fcdca1818fef0b3dd
MATHEUS PESCH ZENERE <matheus.zenere@univille.br>
2026-06-02 21:32:26 -0300

    Criação da tabela Pessoas

--- database/atendelab.sql
+++ database/atendelab.sql
@@ -5,20 +5,29 @@ COLLATE utf8mb4_general_ci;
 USE atendelab;
 
 CREATE TABLE usuarios (
- id INT AUTO_INCREMENT PRIMARY KEY,
- nome VARCHAR(100) NOT NULL,
- email VARCHAR(100) NOT NULL UNIQUE,
- senha VARCHAR(255) NOT NULL,
- perfil ENUM('admin', 'atendente') DEFAULT 'atendente',
- status ENUM('ativo', 'inativo') DEFAULT 'ativo',
- criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    perfil ENUM('admin', 'atendente') DEFAULT 'atendente',
+    status ENUM('ativo', 'inativo') DEFAULT 'ativo',
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
+/* TESTE DA TABELA USUARIO */
 INSERT INTO usuarios (nome, email, senha, perfil, status)
 VALUES (
- 'Administrador',
- 'admin@atendelab.com',
- '$2y$10$J9P2kU2BAMZ3TZcuxTsW4e1D/lka8EocYHzvyoOZmCNcWDQz3RuVC',
- 'admin',
- 'ativo'
+    'Administrador',
+    'admin@atendelab.com',
+    '$2y$10$J9P2kU2BAMZ3TZcuxTsW4e1D/lka8EocYHzvyoOZmCNcWDQz3RuVC',
+    'admin',
+    'ativo'
+);
+
+CREATE TABLE pessoas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    telefone VARCHAR(14) NOT NULL,
+    cpf VARCHAR(14) NOT NULL UNIQUE
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
